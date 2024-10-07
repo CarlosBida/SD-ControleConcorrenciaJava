7108416fdfc055d36e454fb8afcdeee6bb41865a
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2024-10-07 03:35:00 +0000

    Inicial

---
+++ Conta.java
@@ -0,0 +1,13 @@
+public class Conta {
+    public double saldo = 0;
+    public Conta(double saldo) {
+        this.saldo = saldo;
+        System.out.println("Conta criada. Saldo inicial: R$" + saldo);
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
